2ad6b6b416b7d9dc9a53b5ad3aaebd709efebe33
TK-RT <r.takemura117@gmail.com>
2020-07-01 20:43:33 +0900

    top controller追加

---
+++ app/controllers/users/top_controller.rb
@@ -0,0 +1,5 @@
+class Users::TopController < ApplicationController
+	def top
+	end
+
+end
---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,6 @@
+class Users::UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+end
