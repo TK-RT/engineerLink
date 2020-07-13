7365ac9bbe42c6c3592fd1d6638569bd6f87be1d
TK-RT <r.takemura117@gmail.com>
2020-07-13 21:29:53 +0900

    機能修正2

--- db/migrate/20200628110801_create_questions.rb
+++ db/migrate/20200628110801_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-      t.references :user, foreign_key: true
+      t.references :user, type: :bigint, foreign_key: true
       t.references :programming_language, foreign_key: true
       t.string :question_title, index: true
       t.text :question_body
