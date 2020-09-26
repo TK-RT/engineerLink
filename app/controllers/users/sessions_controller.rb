a101ea3fdaab0b6ca76bb54625e668e1f8bddf41
TK-RT <r.takemura117@gmail.com>
2020-09-27 08:40:38 +0900

    ゲストログイン機能追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,14 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def new_guest
+    user = User.find(1)
+    user.update(email: 'guest@example.com',last_name: 'ゲスト',first_name: 'ユーザー') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
     :sessions => 'admins/sessions',
     :passwords => 'admins/passwords'
   }
+
   devise_for :users, :controllers => {
   	:registrations => 'users/registrations',
     :sessions => 'users/sessions',
     :passwords => 'users/passwords'
   }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admins do
