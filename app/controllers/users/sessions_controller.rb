73793fda3f549d76f80ec46f1eee4c438947b075
TK-RT <r.takemura117@gmail.com>
2020-10-06 11:55:35 +0900

    機能修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -27,7 +27,17 @@ class Users::SessionsController < Devise::SessionsController
 
   def new_guest
     user = User.find(1)
-    user.update(email: 'guest@example.com',last_name: 'ゲスト',first_name: 'ユーザー') do |user|
+    user.update(email: 'guest@example.com',
+                last_name: 'ゲスト',
+                first_name: 'ユーザー',
+                last_name_kana: 'ゲスト',
+                first_name_kana: 'ユーザー',
+                birthday: '2020-1-1',
+                gender: '男性',
+                postcode: '1112222',
+                prefecture: '東京都',
+                address: '千代田区',
+                phone_number: '00000000000') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
     sign_in user
